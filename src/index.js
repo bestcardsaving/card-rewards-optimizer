aa0ffd20fa232ea0b98ba9639da89a6eca087049
bestcardsaving <bestcardsaving@gmail.com>
2026-04-25 21:36:30 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './index.jsx';
+import App from './index.jsx'; // Make sure this matches your filename exactly!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
